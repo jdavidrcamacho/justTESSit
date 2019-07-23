6e5f6f3de80ba27131acfc5a1444c01aed1f4291
jdavidrcamacho <jdavidrcamacho@gmail.com>
2019-07-23 09:08:14 +0100

    we can now check for individual stars

--- justtessit/example.py
+++ justtessit/example.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from justtessit.observations import checkList
+from justtessit.observations import checkList, checkStar
 
 #example with 5 stars
 example_file = "utils/list_of_stars.txt"
 
-#lets check the list
+#lets check the list with checkList
 results = checkList(example_file)
 
 #and print our final results
 print(results)
+
+#We can also check a individual star with checkStar
+result = checkStar('Glise581')
+
+#and print the result
+print(result)
--- justtessit/observations.py
+++ justtessit/observations.py
@@ -5,12 +5,13 @@ from tess_stars2px import tess_stars2px_function_entry
 from astropy.coordinates import SkyCoord
 
 
-def checkList(star_file, header=0):
+def checkList(star_file, header = 0, savefile = True):
     """
-        Returns the a file with TESS observations for a given list of stars
+        Returns the TESS observations for a given list of stars
         Parameters:
             star_file = file with the list of stars
             header = number of lines used as header
+            savefile = True if we want a txt file with the schedule
     """
     #our stars
     stars = np.loadtxt(star_file, skiprows=0, unpack=True, dtype='str')
@@ -46,6 +47,53 @@ def checkList(star_file, header=0):
                       " to ", sectors[int(observed_sectors[i][-1])-1, 2]]
             result = "".join(result)
             allStarList[i].append(result)
+    
+    #now a final list with start and end observations
+    data = np.array(allStarList)
+    if savefile:
+        np.savetxt('star_observations.txt', data, 
+                   delimiter='\t', comments='', fmt='%s')
+    return data
+
+def checkStar(star, savefile = True):
+    """
+        Returns the a file with TESS observations for a given star
+        Parameters:
+            star = name of the star
+            savefile = True if we want a txt file with the schedule
+    """
+    #our coordinates
+    ra, dec = _checkStarCoordinates(star)
+    #giving ids to the stars
+    star_id = [1]
+    
+    #Lets see if TESS will look at it
+    outID, outEclipLong, outEclipLat, \
+    outSec, outCam, outCcd, \
+    outColPix, outRowPix, scinfo = tess_stars2px_function_entry(star_id,ra,dec)
+    
+    observed_sectors = []
+    for i, j in enumerate(star_id):
+        sector = []
+        for ii, jj in enumerate(outID):
+            if j==jj:
+                sector.append(outSec[ii])
+        observed_sectors.append(sector)
+    
+    #date of observation for each sector
+    sectors = np.loadtxt("utils/sectors.txt", skiprows=0, unpack=True, dtype='str')
+    sectors = sectors.T
+    
+    #because I prefer the name of the start instead of RA and DEC
+    allStarList = [[i] for i in [star]]
+    for i, j in enumerate([star]):
+        if len(observed_sectors[i]) == 0:
+            allStarList[i].append("Star not observed")
+        else:
+            result = ["Observed from ", sectors[int(observed_sectors[i][0])-1,1], 
+                      " to ", sectors[int(observed_sectors[i][-1])-1, 2]]
+            result = "".join(result)
+            allStarList[i].append(result)
             
     #now a final list with start and end observations
     data = np.array(allStarList)
@@ -54,7 +102,7 @@ def checkList(star_file, header=0):
     return data
 
 
-def _checkListCoordinates(star_file, header=0):
+def _checkListCoordinates(star_file, header = 0):
     """
         Returns the RA and DEC for a given list of stars
         Parameters:
@@ -73,3 +121,21 @@ def _checkListCoordinates(star_file, header=0):
     dec = np.array(dec)
     dec.reshape(dec.size)
     return ra, dec
+
+def _checkStarCoordinates(star):
+    """
+        Returns the RA and DEC for a given list of stars
+        Parameters:
+            star = name of the star
+    """
+    #astropy RA and DEC for our stars
+    ra, dec = [], []
+    for i,j in enumerate([star]):
+        ra.append(np.array(SkyCoord.from_name(j).ra))
+        dec.append(np.array(SkyCoord.from_name(j).dec))
+    ra = np.array(ra)
+    ra.reshape(ra.size)
+    dec = np.array(dec)
+    dec.reshape(dec.size)
+    return ra, dec
+
