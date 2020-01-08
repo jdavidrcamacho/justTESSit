d38d01bb1b6f0be146c0b8cf95de0f4e86deb934
jdavidrcamacho <joao.camacho@astro.up.pt>
2020-01-08 11:24:53 +0000

    travis test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(name='justessit',
+setup(name='justtessit',
       version='0.1',
       description='Tiny script to see if a given star is going to be observed with TESS',
       author='João Camacho',
